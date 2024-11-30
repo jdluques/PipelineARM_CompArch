0e12942d6ae49ebc95e3e291951c4a695c10459e
Fabrizzio20k <fabrizzio785@gmail.com>
2024-11-30 18:39:03 -0500

    Add str and ldr instructions

--- PIPELINE_ARCH.srcs/sources_1/new/imem.v
+++ PIPELINE_ARCH.srcs/sources_1/new/imem.v
@@ -12,8 +12,12 @@ module imem (
     RAM[2] = 32'hE2411001;  // Instruccion 2
     RAM[3] = 32'hE2511000;  // Instruccion 3
     RAM[4] = 32'hCAFFFFFC;  // Instruccion 4
-    RAM[5] = 32'hEAFFFFFE;  // BUCLE INFINITO
-    for (i = 6; i < 256; i = i + 1) begin
+    RAM[5] = 32'hE2802002;  // Instruccion 5
+    RAM[6] = 32'hE280000C;  // Instruccion 6
+    RAM[7] = 32'hE5802004;  // Instruccion 7
+    RAM[8] = 32'hE5903004;  // Instruccion 8
+    RAM[9] = 32'hEAFFFFFE;  // BUCLE INFINITO
+    for (i = 10; i < 256; i = i + 1) begin
       RAM[i] = 32'h00000000;
     end
   end
