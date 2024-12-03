75a2019b40f7b6c2c8091cf5b169b9cad4427061
Fabrizzio20k <fabrizzio785@gmail.com>
2024-12-03 10:28:03 -0500

    Add extra instructions

--- PIPELINE_ARCH.srcs/sources_1/new/alu.v
+++ PIPELINE_ARCH.srcs/sources_1/new/alu.v
@@ -24,6 +24,8 @@ module alu (
       5'b00010: Result = a & b;
       5'b00011: Result = a | b;
       5'b00100: Result = b;
+      5'b01001: Result = a ^ b;
+      5'b01010: Result = a | (~b);
     endcase
   end
   assign neg = Result[31];
